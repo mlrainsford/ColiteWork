0485234fd7d07879148f1f049e73b81762eff7ae
Mitchell Rainsford <mlrainsford@gmail.com>
2017-12-14 12:57:53 -0500

    Initial commit

---
+++ DailyServerApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyServerApplication
+{
+    /// <summary>
+    /// Main class used to start the application
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
